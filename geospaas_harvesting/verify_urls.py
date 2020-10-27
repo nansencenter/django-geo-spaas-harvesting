96bbffbd56ce28f1142d5ae6fba9e6d23247f778
Adrien Perrin <adrien.perrin@nersc.no>
2020-10-27 14:24:13 +0000

    update verify_urls.py script

--- geospaas_harvesting/verify_urls.py
+++ geospaas_harvesting/verify_urls.py
@@ -1,29 +1,68 @@
 """ Verification module. """
+from geospaas.catalog.models import DatasetURI
+import concurrent.futures
+import logging
 import os
 import sys
 from datetime import datetime
+from threading import Lock
 
 import django
 import requests
+import requests.auth
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geospaas_harvesting.settings')
 django.setup()
 
-from geospaas.catalog.models import DatasetURI
+
+logging.basicConfig(level=logging.INFO)
+
+providers = {
+    'scihub': (
+        'https://scihub.copernicus.eu',
+        requests.auth.HTTPBasicAuth('topvoys', os.environ.get('COPERNICUS_OPEN_HUB_PASSWORD'))
+    ),
+    'podaac': ('https://opendap.jpl.nasa.gov/opendap/', None)
+}
+
+
+def check_url(lock, file_name, dataset_uri, auth):
+    """"""
+    response = requests.head(dataset_uri.uri, allow_redirects=True, auth=auth)
+    logging.debug("%d %s", response.status_code, dataset_uri.uri)
+    if response.status_code < 200 or response.status_code > 299:
+        with lock:
+            with open(file_name, 'w') as file_handle:
+                file_handle.write(f"{response.status_code} {dataset_uri.uri} {os.linesep}")
+
+
+def check_provider_urls(file_name, url_prefix, auth):
+    """"""
+    logging.info("Starting to check %s URLs", url_prefix)
+    lock = Lock()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=50) as thread_executor:
+        # with open(file_name, 'w') as file_handle:
+        for dataset_uri in DatasetURI.objects.filter(uri__startswith=url_prefix).iterator():
+            thread_executor.submit(check_url, lock, file_name, dataset_uri, auth)
+    logging.info("Finished checking %s URLs", url_prefix)
+
 
 def main():
-    """ Verifies the datasets based on their dataseturi. If the download link does not provide a
-    download availability and returns a response that does not start with '2' in its status code,
-    then the dataset uri is written into a file named "filename"."""
+    """"""
     try:
-        filename = sys.argv[1]
+        dir_name = sys.argv[1]
     except IndexError:
-        filename = f"unverified_datasets_at_{datetime.now().strftime('%Y-%m-%d___%H_%M_%S')}"
-    with open(filename + ".txt", 'w') as f:
-        for dsuri in DatasetURI.objects.iterator():
-            response = requests.head(dsuri.uri, allow_redirects=True)
-            if response.status_code < 200 or response.status_code > 299:
-                f.write(dsuri.uri + os.linesep)
+        dir_name = '.'
+
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        for file_prefix, (url_prefix, auth) in providers.items():
+            file_name = os.path.join(
+                dir_name,
+                f"{file_prefix}_stale_urls_{datetime.now().strftime('%Y-%m-%dT%H:%M:%S')}.txt"
+            )
+            executor.submit(check_provider_urls, file_name, url_prefix, auth)
+
+    logging.info("Finished checking all URLs")
 
 
 if __name__ == '__main__':
