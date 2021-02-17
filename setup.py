ea59def13f7626e57fad8dea471ca84b972fca9d
arash <62345618+aanersc@users.noreply.github.com>
2021-02-17 10:08:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(os.path.join(os.path.dirname(__file__), "README.md"), "r") as fh:
 
 setuptools.setup(
     name="geospaas_harvesting",
-    version="3.0.1",
+    version="3.0.2",
     author="Adrien Perrin",
     author_email="adrien.perrin@nersc.no",
     description="Metadata harvesting tool for GeoSPaaS",
