138e9cee854b02897a88e682fabc75031424d82f
Adrien Perrin <adrien.perrin@nersc.no>
2021-05-03 13:36:41 +0200

    remove throttle decorator + limit pool queue

--- geospaas_harvesting/verify_urls.py
+++ geospaas_harvesting/verify_urls.py
@@ -1,12 +1,12 @@
 """ Verification module. """
 import concurrent.futures
-import functools
 import logging
 import os
 import sys
 import time
+from contextlib import closing
 from datetime import datetime
-from threading import Lock
+from threading import BoundedSemaphore, Lock
 
 import django
 import oauthlib.oauth2
@@ -19,26 +19,30 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geospaas_harvesting.settings')
 django.setup()
 from geospaas.catalog.models import DatasetURI
 
-
 logging.basicConfig(level=logging.INFO)
 
 
-def throttle(period):
-    """Decorator builder. Returns a decorator which prevents a function
-    from being called more than once every `period` seconds
+class BoundedThreadPoolExecutor(concurrent.futures.ThreadPoolExecutor):
+    """A ThreadPoolExecutor which has a limit on the number of jobs
+    which can be submitted at once
     """
-    def decorator_throttle(func):
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            if wrapper.last_call:
-                time_since_last_call = time.monotonic() - wrapper.last_call
-                if time_since_last_call < period:
-                    time.sleep(period - time_since_last_call)
-            wrapper.last_call = time.monotonic()
-            return func(*args, **kwargs)
-        wrapper.last_call = 0
-        return wrapper
-    return decorator_throttle
+
+    def __init__(self, *args, queue_limit=10000, **kwargs):
+        self.semaphore = BoundedSemaphore(kwargs.get('max_workers', 0) + queue_limit)
+        super().__init__(*args, **kwargs)
+
+    def submit(self, fn, *args, **kwargs):
+        self.semaphore.acquire()
+        try:
+            future = super().submit(fn, *args, **kwargs)
+        except:
+            # if anything goes, wrong, we need to release the semaphore
+            self.semaphore.release()
+            raise
+        else:
+            # release the semaphore once the thread ends
+            future.add_done_callback(lambda x: self.semaphore.release())
+            return future
 
 
 def build_oauth2(username, password, token_url, client_id):
@@ -69,7 +73,8 @@ def get_auth(attributes):
 
 
 def read_config(config_path):
-    """Reads the configuration file and build a providers dictionary
+    """Reads the configuration file and builds a dictionary of
+    providers
     """
     yaml.SafeLoader.add_constructor('!ENV', lambda loader, node: os.getenv(node.value))
     with open(config_path, 'r') as config_file:
@@ -77,41 +82,37 @@ def read_config(config_path):
 
     providers = {}
     for provider, attributes in config.items():
-
-        throttle_period = attributes.get('throttle')
-        checker_function = throttle(throttle_period)(check_url) if throttle_period else check_url
-
         providers[provider] = {
             'url': attributes['url'],
             'auth': get_auth(attributes),
-            'checker': checker_function
+            'throttle': attributes.get('throttle', 0)
         }
 
     return providers
 
 
-def check_url(lock, file_name, dataset_uri, auth):
+def check_url(lock, file_name, dataset_uri, auth, throttle=0):
     """Sends an HTTP HEAD request to the URL and writes the URL to the
     output file if it fails
     """
-    response = requests.head(dataset_uri.uri, allow_redirects=True, auth=auth)
-    logging.debug("%d %s", response.status_code, dataset_uri.uri)
-    if response.status_code < 200 or response.status_code > 299:
-        with lock:
-            with open(file_name, 'a') as file_handle:
-                file_handle.write(
-                    f"{response.status_code} {dataset_uri.id} {dataset_uri.uri} {os.linesep}"
-                    f"{response.text} {os.linesep}{os.linesep}"
-                )
+    with closing(requests.head(dataset_uri.uri, allow_redirects=True, auth=auth)) as response:
+        logging.debug("%d %s", response.status_code, dataset_uri.uri)
+        if response.status_code < 200 or response.status_code > 299:
+            with lock:
+                with open(file_name, 'a') as file_handle:
+                    file_handle.write(
+                        f"{response.status_code} {dataset_uri.id} {dataset_uri.uri}{os.linesep}")
+    time.sleep(throttle)
 
 
-def check_provider_urls(file_name, checker, url_prefix, auth):
+def check_provider_urls(file_name, url_prefix, auth, throttle=0):
     """Check the URLs for one provider"""
     logging.info("Starting to check %s URLs", url_prefix)
     lock = Lock()
-    with concurrent.futures.ThreadPoolExecutor(max_workers=50) as thread_executor:
+    max_workers = 1 if throttle else 50
+    with BoundedThreadPoolExecutor(max_workers=max_workers, queue_limit=2000) as thread_executor:
         for dataset_uri in DatasetURI.objects.filter(uri__startswith=url_prefix).iterator():
-            thread_executor.submit(checker, lock, file_name, dataset_uri, auth)
+            thread_executor.submit(check_url, lock, file_name, dataset_uri, auth, throttle=throttle)
     logging.info("Finished checking %s URLs", url_prefix)
 
 
@@ -127,20 +128,32 @@ def main():
     providers = read_config(os.path.join(os.path.dirname(__file__), 'check.yml'))
 
     with concurrent.futures.ProcessPoolExecutor() as executor:
+        futures = {}
         for provider, attributes in providers.items():
             results_file_name = os.path.join(
                 dir_name,
                 f"{provider}_stale_urls_{datetime.now().strftime('%Y-%m-%dT%H:%M:%S')}.txt"
             )
-            executor.submit(
+            futures[executor.submit(
                 check_provider_urls,
                 results_file_name,
-                attributes['checker'],
                 attributes['url'],
-                attributes['auth']
-            )
+                attributes['auth'],
+                attributes['throttle']
+            )] = attributes['url']
 
-    logging.info("Finished checking all URLs")
+        error = False
+        for future in concurrent.futures.as_completed(futures):
+            try:
+                future.result()
+            except Exception:
+                error = True
+                logging.error("An error occurred while checking '%s'",
+                              futures[future], exc_info=True)
+    if error:
+        sys.exit(1)
+    else:
+        logging.info("Finished checking all URLs")
 
 
 if __name__ == '__main__':
