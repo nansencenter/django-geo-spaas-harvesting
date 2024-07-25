cd757f072573c66865e5c0b0a39af08490766f92
Adrien Perrin <adrien.perrin@nersc.no>
2024-07-25 12:52:29 +0000

    update tests for cmems provider

--- tests/providers/test_cmems.py
+++ tests/providers/test_cmems.py
@@ -1,33 +1,35 @@
 # pylint: disable=protected-access
 """Tests for the CMEMS provider"""
 import unittest
-import unittest.mock as mock
 from datetime import datetime, timezone
 
-import geospaas_harvesting.crawlers as crawlers
-from geospaas_harvesting.providers.cmems import CMEMSFTPProvider
+from geospaas_harvesting.providers.cmems import CMEMSProvider, CMEMSCrawler
 
 
-class CMEMSFTPProviderTestCase(unittest.TestCase):
-    """Tests for CMEMSFTPProvider"""
+class CMEMSProviderTestCase(unittest.TestCase):
+    """Tests for CMEMSProvider"""
 
     def test_make_crawler(self):
         """Test creating a crawler from parameters"""
-        provider = CMEMSFTPProvider(name='test', username='user', password='pass')
+        provider = CMEMSProvider(name='test', username='user', password='pass')
         parameters = {
             'start_time': datetime(2023, 1, 1, tzinfo=timezone.utc),
             'end_time': datetime(2023, 1, 2, tzinfo=timezone.utc),
-            'server': 'nrt',
-            'directory': '/Core/MULTIOBS_GLO_PHY_NRT_015_003/foo',
-            'include': '.*'
+            'product_id': 'SEALEVEL_GLO_PHY_L3_NRT_008_044',
+            'dataset_ids': [
+                'cmems_obs-sl_glo_phy-ssh_nrt_al-l3-duacs_PT1S',
+                'cmems_obs-sl_glo_phy-ssh_nrt_al-l3-duacs_PT1S',
+            ],
         }
-        with mock.patch('ftplib.FTP'):
-            self.assertEqual(
-                provider._make_crawler(parameters),
-                crawlers.FTPCrawler(
-                    'ftp://nrt.cmems-du.eu/Core/MULTIOBS_GLO_PHY_NRT_015_003/foo',
-                    include='.*',
-                    time_range=(datetime(2023, 1, 1, tzinfo=timezone.utc),
-                                datetime(2023, 1, 2, tzinfo=timezone.utc)),
-                    username='user',
-                    password='pass'))
+        self.assertEqual(
+            provider._make_crawler(parameters),
+            CMEMSCrawler(
+                'SEALEVEL_GLO_PHY_L3_NRT_008_044',
+                [
+                    'cmems_obs-sl_glo_phy-ssh_nrt_al-l3-duacs_PT1S',
+                    'cmems_obs-sl_glo_phy-ssh_nrt_al-l3-duacs_PT1S',
+                ],
+                time_range=(datetime(2023, 1, 1, tzinfo=timezone.utc),
+                            datetime(2023, 1, 2, tzinfo=timezone.utc)),
+                username='user',
+                password='pass'))
