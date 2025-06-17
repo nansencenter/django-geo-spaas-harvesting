23e9e2425679b04381c9536015237297e326a3ad
Adrien Perrin <adrien.perrin@nersc.no>
2025-06-17 13:38:28 +0000

    fix tag handling in ingester

--- geospaas_harvesting/ingesters.py
+++ geospaas_harvesting/ingesters.py
@@ -65,6 +65,7 @@ class Ingester():
             for parameter in parameters:
                 dataset.parameters.add(parameter)
             for tag in tags:
+                tag.save()
                 dataset.tags.add(tag)
 
         return (dataset_uri.uri, dataset.entry_id, dataset_status, dataset_uri_status)
