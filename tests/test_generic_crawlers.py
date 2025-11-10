b175b4426619ca8c5ae03f92456583bcf8de3cc0
Adrien Perrin <adrien.perrin@nersc.no>
2025-11-10 09:04:07 +0000

    fix HTTPPaginatedAPICrawlerTestCase

--- tests/test_generic_crawlers.py
+++ tests/test_generic_crawlers.py
@@ -998,51 +998,58 @@ class HTTPPaginatedAPICrawlerTestCase(unittest.TestCase):
 
     def test_equality(self):
         """Test the equality operator between crawlers"""
-        self.assertEqual(crawlers.HTTPPaginatedAPICrawler('http://foo'),
-                         crawlers.HTTPPaginatedAPICrawler('http://foo'))
         self.assertEqual(
-            crawlers.HTTPPaginatedAPICrawler('http://foo',
-                                    username='user', password='pass', search_terms={'bar': 'baz'}),
-            crawlers.HTTPPaginatedAPICrawler('http://foo',
-                                    username='user', password='pass', search_terms={'bar': 'baz'}))
-        self.assertNotEqual(crawlers.HTTPPaginatedAPICrawler('http://foo'),
-                            crawlers.HTTPPaginatedAPICrawler('http://bar'))
+            crawlers_paginated_api.HTTPPaginatedAPICrawler.from_kwargs(url='http://foo'),
+            crawlers_paginated_api.HTTPPaginatedAPICrawler.from_kwargs(url='http://foo'))
+        self.assertEqual(
+            crawlers_paginated_api.HTTPPaginatedAPICrawler.from_kwargs(
+                url='http://foo', username='user', password='pass', search_terms={'bar': 'baz'}),
+            crawlers_paginated_api.HTTPPaginatedAPICrawler.from_kwargs(
+                url='http://foo', username='user', password='pass', search_terms={'bar': 'baz'}))
+        self.assertNotEqual(
+            crawlers_paginated_api.HTTPPaginatedAPICrawler.from_kwargs(url='http://foo'),
+            crawlers_paginated_api.HTTPPaginatedAPICrawler.from_kwargs(url='http://bar'))
 
     def test_get_page_size(self):
         """Test page_size getter"""
         with mock.patch(
-                'geospaas_harvesting.crawlers.HTTPPaginatedAPICrawler.PAGE_SIZE_NAME', 'size'):
-            self.assertEqual(
-                crawlers.HTTPPaginatedAPICrawler('https://foo', page_size=10).page_size,
-                10)
+                'geospaas_harvesting.crawlers.paginated_api.HTTPPaginatedAPICrawler.PAGE_SIZE_NAME',
+                'size'):
+            crawler = crawlers_paginated_api.HTTPPaginatedAPICrawler.from_kwargs(
+                url='https://foo', page_size=10)
+            crawler.set_initial_state()
+            self.assertEqual(crawler.page_size, 10)
 
     def test_get_page_offset(self):
         """Test page_offset getter"""
-        self.assertEqual(crawlers.HTTPPaginatedAPICrawler('foo', initial_offset=10).page_offset, 10)
+        crawler = crawlers_paginated_api.HTTPPaginatedAPICrawler.from_kwargs(
+                url='foo', initial_offset=10)
+        crawler.set_initial_state()
+        self.assertEqual(crawler.page_offset, 10)
 
     def test_set_page_offset(self):
         """Test page_offset setter"""
-        crawler = crawlers.HTTPPaginatedAPICrawler('https://foo')
+        crawler = crawlers_paginated_api.HTTPPaginatedAPICrawler.from_kwargs(url='https://foo')
         crawler.page_offset = 12
         self.assertEqual(crawler.page_offset, 12)
 
     def test_set_initial_state(self):
         """Test that set_initial_state correctly resets the crawler"""
-        crawler = crawlers.HTTPPaginatedAPICrawler('https://foo')
+        crawler = crawlers_paginated_api.HTTPPaginatedAPICrawler.from_kwargs(url='https://foo')
         # Set non-default offset and _urls values
         crawler.request_parameters['params'][crawler.PAGE_OFFSET_NAME] = 200
-        crawler._results = ['url1', 'url2']
+        crawler.page_offset = 15
 
         crawler.set_initial_state()
         self.assertEqual(
             crawler.request_parameters['params'][crawler.PAGE_OFFSET_NAME], crawler.initial_offset)
-        self.assertListEqual(crawler._results, [])
 
     def test_get_next_page(self):
         """_get_next_page() should get the page at the current offset,
         then increment the offset
         """
-        crawler = crawlers.HTTPPaginatedAPICrawler('https://foo')
+        crawler = crawlers_paginated_api.HTTPPaginatedAPICrawler.from_kwargs(url='https://foo')
+        crawler.set_initial_state()
         response = requests.Response()
         response.status_code = 200
         response.raw = io.BytesIO(b'foo')
@@ -1055,17 +1062,46 @@ class HTTPPaginatedAPICrawlerTestCase(unittest.TestCase):
         """_get_datasets_info() should raise a NotImplementedError
         when called directly from HTTPPaginatedAPICrawler
         """
-        crawler = crawlers.HTTPPaginatedAPICrawler('foo')
+        crawler = crawlers_paginated_api.HTTPPaginatedAPICrawler.from_kwargs(url='foo')
         with self.assertRaises(NotImplementedError):
             crawler._get_datasets_info('')
 
+    def test_abstract_get_entries(self):
+        """_get_entries() should raise a NotImplementedError
+        when called directly from HTTPPaginatedAPICrawler
+        """
+        crawler = crawlers_paginated_api.HTTPPaginatedAPICrawler.from_kwargs(url='foo')
+        with self.assertRaises(NotImplementedError):
+            crawler._get_entries('')
+
     def test_crawl(self):
         """Test the crawling mechanism for HTTP paginated APIs"""
-        crawler = crawlers.HTTPPaginatedAPICrawler('https://foo')
-        crawler._results = [crawlers.DatasetInfo('bar')]
-        with mock.patch.object(crawler, '_get_datasets_info', return_value=False), \
-                mock.patch.object(crawler, '_get_next_page'):
-            self.assertListEqual(list(crawler.crawl()), [crawlers.DatasetInfo('bar')])
+        class TestHTTPAPICrawler(crawlers_paginated_api.HTTPPaginatedAPICrawler):
+            def __init__(self, **kwargs):
+                super().__init__(**kwargs)
+                self.ran = False
+
+            def _get_next_page(self):
+                if self.ran:
+                    return ''
+                else:
+                    self.ran = True
+                    return 'https://foo/bar.nc;https://foo/baz.nc'
+
+            def _get_entries(self, page:str):
+                if page:
+                    return page.split(';')
+                else:
+                    return None
+
+            def _get_datasets_info(self, entries):
+                return [crawlers_base.DatasetInfo(url) for url in entries]
+
+        crawler = TestHTTPAPICrawler.from_kwargs(url='https://foo')
+        self.assertListEqual(
+            list(crawler.crawl()),
+            [crawlers_base.DatasetInfo('https://foo/bar.nc'),
+             crawlers_base.DatasetInfo('https://foo/baz.nc')])
 
 
 class FTPCrawlerTestCase(unittest.TestCase):
