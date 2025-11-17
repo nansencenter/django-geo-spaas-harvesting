226ee5ccaf5a984055cc9869d18898e698b559e6
Adrien Perrin <adrien.perrin@nersc.no>
2025-11-17 11:04:56 +0000

    use str paths in iter_modules
    for compatibility with python3.10

--- geospaas_harvesting/crawlers/__init__.py
+++ geospaas_harvesting/crawlers/__init__.py
@@ -6,7 +6,7 @@ from .base import Crawler
 from ..utils import get_all_subclasses
 
 
-for (_, name, _) in pkgutil.iter_modules([Path(__file__).parent]):
+for (_, name, _) in pkgutil.iter_modules([str(Path(__file__).parent)]):
         importlib.import_module('.' + name, __package__)
 
 # index that allows to retrieve a normalizer class using its name
--- geospaas_harvesting/normalizers/__init__.py
+++ geospaas_harvesting/normalizers/__init__.py
@@ -11,7 +11,7 @@ from .base import MetadataNormalizer, StreamMetadataNormalizer
 from ..utils import get_all_subclasses
 
 
-for (_, name, _) in pkgutil.iter_modules([Path(__file__).parent]):
+for (_, name, _) in pkgutil.iter_modules([str(Path(__file__).parent)]):
         importlib.import_module('.' + name, __package__)
 
 # index that allows to retrieve a normalizer class using its name
