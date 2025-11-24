ca944d8557756f2e84778ed4bd7f55becb763ac7
Adrien Perrin <adrien.perrin@nersc.no>
2025-11-24 11:27:01 +0000

    add EarthdataCMRCrawler tests

--- tests/crawlers/test_paginated_api.py
+++ tests/crawlers/test_paginated_api.py
@@ -1,10 +1,13 @@
 import io
+import json
 import logging
 import unittest
 import unittest.mock as mock
-from datetime import datetime
+from datetime import datetime, timezone
+from pathlib import Path
 
 import requests
+import shapely.geometry
 
 import geospaas_harvesting.crawlers.base as crawlers_base
 import geospaas_harvesting.crawlers.paginated_api as crawlers_paginated_api
@@ -138,3 +141,216 @@ class HTTPPaginatedAPICrawlerTestCase(unittest.TestCase):
             list(crawler.crawl()),
             [crawlers_base.DatasetInfo('https://foo/bar.nc'),
              crawlers_base.DatasetInfo('https://foo/baz.nc')])
+
+
+class EarthdataCMRCrawlerTestCase(unittest.TestCase):
+    """Tests for EarthdataCMRCrawler"""
+
+    def setUp(self):
+        self.crawler = crawlers_paginated_api.EarthDataCMRCrawler.from_kwargs(
+            url='foo',
+            search_terms={'short_name': 'COLLECTION', 'platform': 'value1', 'instrument': 'value2'})
+
+    def test_build_request_parameters_no_argument(self):
+        """Test building the request parameters without specifying any argument"""
+        self.assertDictEqual(self.crawler._build_request_parameters(), {
+            'params': {
+                'page_size': 100,
+                'page_num': 1,
+                'sort_key': '+start_date',
+            }
+        })
+
+    def test_build_request_parameters_no_time_range(self):
+        """Test building the request parameters without time range"""
+        self.assertDictEqual(
+            self.crawler._build_request_parameters(
+                {'short_name': 'COLLECTION', 'platform': 'value1', 'instrument': 'value2'}),
+            {
+                'params': {
+                    'platform': 'value1',
+                    'instrument': 'value2',
+                    'short_name': 'COLLECTION',
+                    'page_size': 100,
+                    'page_num': 1,
+                    'sort_key': '+start_date'
+                }
+            })
+
+    def test_build_request_parameters_with_time_range(self):
+        """Test building the request parameters without time range"""
+        time_range = (
+            datetime(2020, 2, 1, tzinfo=timezone.utc),
+            datetime(2020, 2, 2, tzinfo=timezone.utc)
+        )
+
+        self.assertDictEqual(
+            self.crawler._build_request_parameters(
+                {'short_name': 'COLLECTION', 'platform': 'value1', 'instrument': 'value2'},
+                time_range),
+            {
+                'params': {
+                    'platform': 'value1',
+                    'instrument': 'value2',
+                    'short_name': 'COLLECTION',
+                    'page_size': 100,
+                    'page_num': 1,
+                    'sort_key': '+start_date',
+                    'temporal': '2020-02-01T00:00:00+00:00,2020-02-02T00:00:00+00:00'
+                }
+            }
+        )
+
+    def test_build_request_parameters_with_time_range_start_only(self):
+        """Test building the request parameters without time range"""
+        time_range = (datetime(2020, 2, 1, tzinfo=timezone.utc), None)
+
+        self.assertDictEqual(
+            self.crawler._build_request_parameters(
+                {'short_name': 'COLLECTION', 'platform': 'value1', 'instrument': 'value2'},
+                time_range),
+            {
+                'params': {
+                    'platform': 'value1',
+                    'instrument': 'value2',
+                    'short_name': 'COLLECTION',
+                    'page_size': 100,
+                    'page_num': 1,
+                    'sort_key': '+start_date',
+                    'temporal': '2020-02-01T00:00:00+00:00,'
+                }
+            })
+
+    def test_build_request_parameters_with_time_range_end_only(self):
+        """Test building the request parameters without time range"""
+        time_range = (None, datetime(2020, 2, 2, tzinfo=timezone.utc))
+
+        self.assertDictEqual(
+            self.crawler._build_request_parameters(
+                {'short_name': 'COLLECTION', 'platform': 'value1', 'instrument': 'value2'},
+                time_range),
+            {
+                'params': {
+                    'platform': 'value1',
+                    'instrument': 'value2',
+                    'short_name': 'COLLECTION',
+                    'page_size': 100,
+                    'page_num': 1,
+                    'sort_key': '+start_date',
+                    'temporal': ',2020-02-02T00:00:00+00:00'
+                }
+            })
+
+    def test_build_request_parameters_with_polygon(self):
+        """Test building the request parameters with a polygon location
+        """
+        self.assertDictEqual(
+            self.crawler._build_request_parameters({'short_name': 'COLLECTION'},
+                location=shapely.geometry.Polygon(((1, 90), (180, 90), (180, 3), (1, 3), (1, 90)))),
+            {
+                'params': {
+                    'short_name': 'COLLECTION',
+                    'page_size': 100,
+                    'page_num': 1,
+                    'sort_key': '+start_date',
+                    'polygon[]': '1.0,89.99,179.99,89.99,179.99,3.0,1.0,3.0,1.0,89.99'
+                }
+            })
+
+    def test_build_request_parameters_with_line(self):
+        """Test building the request parameters with a line location
+        """
+        self.assertDictEqual(
+            self.crawler._build_request_parameters({'short_name': 'COLLECTION'},
+                location=shapely.geometry.LineString(((1, 2), (3, 4), (5, 6)))),
+            {
+                'params': {
+                    'short_name': 'COLLECTION',
+                    'page_size': 100,
+                    'page_num': 1,
+                    'sort_key': '+start_date',
+                    'line[]': '1.0,2.0,3.0,4.0,5.0,6.0'
+                }
+            })
+
+    def test_build_request_parameters_with_point(self):
+        """Test building the request parameters with a point location
+        """
+        self.assertDictEqual(
+            self.crawler._build_request_parameters({'short_name': 'COLLECTION'},
+                location=shapely.geometry.Point((1, 2))),
+            {
+                'params': {
+                    'short_name': 'COLLECTION',
+                    'page_size': 100,
+                    'page_num': 1,
+                    'sort_key': '+start_date',
+                    'point': '1.0,2.0'
+                }
+            })
+
+    def test_build_request_parameters_with_bbox(self):
+        """Test building the request parameters with a bounding box
+        location
+        """
+        self.assertDictEqual(
+            self.crawler._build_request_parameters({'short_name': 'COLLECTION'},
+                location='bounding_box[]=-10,-5,10,5'),
+            {
+                'params': {
+                    'short_name': 'COLLECTION',
+                    'page_size': 100,
+                    'page_num': 1,
+                    'sort_key': '+start_date',
+                    'bounding_box[]': '-10,-5,10,5'
+                }
+            })
+
+    def test_build_request_parameters_location_error(self):
+        """Test building the request parameters with a bounding box
+        location
+        """
+        with self.assertRaises(ValueError):
+            self.crawler._build_request_parameters({'short_name': 'COLLECTION'}, location=1)
+
+    def test_find_download_url(self):
+        """Test finding a download URL in an entry"""
+        entry = {
+            'umm': {
+                'RelatedUrls': [
+                    {'URL': 'https://foo/bar.json', 'Type': 'EXTENDED METADATA'},
+                    {'URL': 'https://foo/bar.nc', 'Type': 'GET DATA'},
+                    {'URL': 'https://baz/bar.nc', 'Type': 'GET DATA'},
+                    {'URL': 'https://foo/qux.png', 'Type': 'DIRECT DOWNLOAD'},
+                ]
+            }
+        }
+        self.assertEqual(self.crawler._find_download_url(entry), 'https://foo/bar.nc')
+
+    def test_find_download_url_no_get_data(self):
+        """Test finding a download URL in an entry when no GET DATA type is available"""
+        entry = {
+            'umm': {
+                'RelatedUrls': [
+                    {'URL': 'https://foo/bar.nc', 'Type': 'DOWNLOAD'},
+                    {'URL': 'https://baz/bar.nc', 'Type': 'DOWNLOAD'},
+                ]
+            }
+        }
+        self.assertEqual(self.crawler._find_download_url(entry), 'https://foo/bar.nc')
+
+    def test_get_datasets_info(self):
+        """_get_datasets_info() should extract datasets information
+        from a response page
+        """
+        data_file_path = str(Path(__file__).parent.parent / 'data/earthdata_cmr/result_page.json')
+
+        with open(data_file_path, 'r') as f_h:
+            page = f_h.read()
+
+        expected_entry = crawlers_base.DatasetInfo(
+            'https://oceandata.sci.gsfc.nasa.gov/cmr/getfile/V2012002205400.L2_SNPP_OC.nc',
+            metadata=json.loads(page)['items'][0])
+
+        dataset_infos = self.crawler._get_datasets_info(self.crawler._get_entries(page))
+        self.assertEqual(next(dataset_infos), expected_entry)
